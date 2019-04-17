2e2e832bf77db69d7d5c44c3b635cc2be0c86243
Nick Webb <nwebb@pivotal.io>
2019-04-17 16:07:57 -0700

    Pin terraforming-azure provider to 1.24.0
    [#165434833] Pin terraforming-azure provider back
    
    Signed-off-by: Shaan Sapra <shsapra@pivotal.io>

--- provider.tf
+++ provider.tf
@@ -3,6 +3,7 @@ provider "azurerm" {
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
   tenant_id       = "${var.tenant_id}"
+  version         = "=1.24.0"
 }
 
 terraform {
