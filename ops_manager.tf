b36abc952781af422fb394ea1ab9556e06b93b1b
Maria Shaldybin <mariash@vmware.com>
2021-02-16 11:26:59 -0800

    Fix typo

--- ops_manager.tf
+++ ops_manager.tf
@@ -50,7 +50,7 @@ resource "azurerm_image" "ops_manager_image" {
   name                = "ops_manager_image"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.pcf_resource_group.name}"
-  count               = "1
+  count               = 1
 
   os_disk {
     os_type  = "Linux"
