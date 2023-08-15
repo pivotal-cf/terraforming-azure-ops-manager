07a6a00c76c3beb35cff803d75e74be1d644ca2a
aram price <pricear@vmware.com>
2023-08-14 17:12:55 -0700

    Address `"public_ip_address_allocation": [DEPRECATED]` issue
    See hashicorp/terraform#21664

--- modules/ops_manager/ops_manager.tf
+++ modules/ops_manager/ops_manager.tf
@@ -188,11 +188,11 @@ variable "optional_ops_manager_image_uri" {
 }
 
 resource "azurerm_public_ip" "optional_ops_manager_public_ip" {
-  name                         = "${var.env_name}-optional-ops-manager-public-ip"
-  location                     = "${var.location}"
-  resource_group_name          = "${var.resource_group_name}"
-  public_ip_address_allocation = "static"
-  count                        = "${min(length(split("", var.optional_ops_manager_image_uri)),1)}"
+  name                = "${var.env_name}-optional-ops-manager-public-ip"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group_name}"
+  allocation_method   = "static"
+  count               = "${min(length(split("", var.optional_ops_manager_image_uri)),1)}"
 }
 
 resource "azurerm_network_interface" "optional_ops_manager_nic" {
